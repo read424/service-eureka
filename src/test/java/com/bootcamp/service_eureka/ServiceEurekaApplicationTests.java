90662c90715ebe4f494a5e790f95595a4bbf6f2a
read424 <read424@gmail.com>
2026-06-22 09:24:21 -0500

    init: Initialize Eureka service microservice with Maven structure

---
+++ src/test/java/com/bootcamp/service_eureka/ServiceEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.service_eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
